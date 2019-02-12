0822c00bf74a1b65f4b2c88d39d6ea649b844126
Kevin Cai <kevincai1979@gmail.com>
2019-02-12 14:01:49 -0800

    add serach bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
   onInputChange(e) {
     e.preventDefault();
   }
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={e => console.log(e.target.value)} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
